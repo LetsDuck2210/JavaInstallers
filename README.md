7498f098a85c2185c931c75f98a46e6b2e306ca6
Joshi <joshua@lipps.de>
2023-05-20 00:39:50 +0200

    created readme and changes & fixes to windows installer

---
+++ README.md
@@ -0,0 +1,14 @@
+# Java Installers
+
+Installers that don't hide shit all over your system
+
+---
+
+## win-inst
+
+** Writes to: **
+- C:\Program Files\Java
+
+** Updated Environment Variables (System-wide) **
+- JAVA_HOME
+- Path
--- win-inst.bat
+++ win-inst.bat
@@ -1,5 +1,13 @@
 @echo off
 
+goto main
+
+:exit
+echo exiting...
+pause
+exit
+
+:main
 net session >nul 2>&1
 if not %errorLevel% == 0 (
 	echo requires elevated privileges
@@ -26,14 +34,18 @@ for /f "tokens=2*" %%A in ('reg query "%key%" /v "%valueName%" ^| findstr /i "%v
 
 if defined p7z ( rem 7-Zip is faster than Expand-Archive
 	echo found 7-Zip at %p7z%
-	echo extracting with 7zip...
-	"%p7z%\7z.exe" x %folder% -o"java-inst-tmp0"
+	"%p7z%\7z.exe" x -bso0 -o"java-inst-tmp0" %folder%
 ) else (
 	echo extracting with powershell...
 	powershell -c Expand-Archive %folder%
 )
 
+if not %errorLevel% == 0 goto exit
+
 powershell -c ls java-inst-tmp0 -Name > java-inst-tmp1
+
+if not %errorLevel% == 0 goto exit
+
 set /p outfolder= < java-inst-tmp1
 echo outfolder: %outfolder%
 del java-inst-tmp1
@@ -42,16 +54,21 @@ set home=C:\Program Files\Java\%outfolder%
 set bin=%home%\bin
 
 echo moving to %home%...
-move java-inst-tmp0\%outfolder% %home%
-setx JAVA_HOME "%home%"
+move java-inst-tmp0\%outfolder% "%home%" > nul
+setx /m JAVA_HOME "%home%" > nul
 rd /s /q java-inst-tmp0
+
+if not %errorLevel% == 0 goto exit
 
 echo updating environment...
 powershell -c "$oldpath = [Environment]::GetEnvironmentVariable('Path', 'Machine'); $bin = '%bin%'; [Environment]::SetEnvironmentVariable('Path', $bin + ';' + $oldpath, 'Machine')"
 
-echo generating jarfile(.jar) support...
-assoc .jar=jarfile
-ftype jarfile=%bin%\javaw.exe -jar %1 %*
+if not %errorLevel% == 0 goto exit
 
+echo generating jarfile(.jar) support...
+assoc .jar=jarfile > nul
+ftype jarfile=%bin%\javaw.exe -jar %1 %* > nul
+
+echo:
 echo done
 pause > nul
